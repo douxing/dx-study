12f6a8148cd4fa0c6c2f11ce291361945475b753
douxing <douxing@zccp.com>
2017-12-14 15:52:10 +0800

    chapter 6

---
+++ rust-lang/rust-book/src/ch06-01-defining-an-enum.rs
@@ -0,0 +1,24 @@
+/*
+
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+*/
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+/*
+enum Option<T> {
+    Some(T),
+    None,
+}
+*/
